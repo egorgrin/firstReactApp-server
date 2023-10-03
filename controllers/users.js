ceada4c782b9e9cbe3d4d661461a4b9484292803
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-10-03 23:04:15 +0500

    Function getUser by id added

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,18 @@ import User from '../models/User.js';
 
 export const getUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find().limit(50);
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: error.message});
+  }
+};
+
+export const getUser = async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const users = await User.findOne({ id: req.params.id })
     res.status(200).json(users);
   } catch (error) {
     console.log(error);
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createUser, getUsers} from '../controllers/users.js';
+import {createUser, getUsers, getUser} from '../controllers/users.js';
 
 
 const router = express.Router();
 
 router.get('/', getUsers);
+router.get('/:id', getUser);
 router.post('/', createUser);
 
 export default router;
